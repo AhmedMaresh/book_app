1022af1e84158dbcd3e2c517bba4ce6e9a7409a9
Ahmed Maresh <mareshahmed06@gmail.com>
2026-06-25 13:57:49 +0300

    add custom list view image item

---
+++ lib/features/home/presentation/views/widgets/custom_list_view_image_item.dart
@@ -0,0 +1,25 @@
+import 'package:bookly/core/utils/assets_data.dart';
+import 'package:flutter/material.dart';
+
+class CustomListViewImageItem extends StatelessWidget {
+  const CustomListViewImageItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .28,
+      child: AspectRatio(
+        aspectRatio: 3 / 4,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            image: const DecorationImage(
+              fit: BoxFit.fill,
+              image: AssetImage(AssetsData.testImage),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
